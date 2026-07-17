6be88f7e649569a73186b020525adc501fa1c014
Abimbola Khalilur-Rahman <khalilrahmanabimbola@gmail.com>
2026-07-17 22:52:28 +0100

    Quiz App Fully functional

--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
 ﻿List<Question> questions = QuizLoader.LoadQuestions("questions.json");
+QuizEngine engine = new QuizEngine(questions);
 
-Console.WriteLine($"Loaded {questions.Count} questions.");
-
-foreach (Question q in questions)
+while (!engine.IsFinished)
 {
-    Console.WriteLine($"- {q.Text} (Category: {q.Category})");
+    Console.WriteLine(engine.CurrentQuestion.Text);
+    for (int i = 0; i < engine.CurrentQuestion.Options.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {engine.CurrentQuestion.Options[i]}");
+    }
+
+    Console.WriteLine("Enter Your Answer: ");
+    int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+    bool isCorrect = engine.SubmitAnswer(userAnswer - 1);
+
+    if (isCorrect)
+    {
+        Console.WriteLine("Correct!");
+    }
+    else
+    {
+        Console.WriteLine("Wrong!");
+    }
+
+    Console.WriteLine();
 }
+
+Console.WriteLine($"Quiz finished! Your score: {engine.Score}/{questions.Count}");
