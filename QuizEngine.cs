394d06cde6cef9ef5245933c9001b22392515fd9
Abimbola Khalilur-Rahman <khalilrahmanabimbola@gmail.com>
2026-07-17 15:54:27 +0100

    Add QuizEngine Class

--- QuizEngine.cs
+++ QuizEngine.cs
@@ -10,4 +10,22 @@ public class QuizEngine
         _currentQuestionIndex = 0;
         _score = 0;
     }
+
+    public bool IsFinished => _currentQuestionIndex >= _questions.Count;
+    public Question CurrentQuestion => _questions[_currentQuestionIndex];
+    public int Score => _score;
+
+    public bool SubmitAnswer(int answer)
+    {
+        Question currentQuestion = _questions[_currentQuestionIndex];
+        bool isCorrect = (answer == currentQuestion.CorrectIndex);
+
+        if (isCorrect)
+        {
+            _score++;
+        }
+
+        _currentQuestionIndex++;
+        return isCorrect;
+    }
 }
